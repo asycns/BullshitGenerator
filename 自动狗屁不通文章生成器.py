27758ba2b26ebef24a579d152bbae40149709536
Undefined <46381867+asycns@users.noreply.github.com>
2020-09-06 19:56:26 +0800

    Finished full mark

--- 自动狗屁不通文章生成器.py
+++ 自动狗屁不通文章生成器.py
@@ -9,11 +9,15 @@ data = readJSON.读JSON文件("data.json")
 前面垫话 = data["before"] # 在名人名言前面弄点废话
 后面垫话 = data['after']  # 在名人名言后面弄点废话
 废话 = data['bosh'] # 代表文章主要废话来源
+synonyms = data['synonyms']
+synonyms_keys = synonyms.keys()
+
+punctuations = '＂＃＄％＆＇（）＊＋，－／：；＜＝＞＠［＼］＾＿｀｛｜｝～｟｠｢｣､\u3000、〃〈〉《》「」『』【】〔〕〖〗〘〙〚〛〜〝〞〟〰〾〿–—‘’‛“”„‟…‧﹏﹑﹔·！？｡。'
 
 xx = "学生会退会"
 
 重复度 = 2
-required_rep = input('Input the amount of junk [2]: ')
+required_rep = input('Input the amount of shit [2]: ')
 if required_rep.isdigit():
     重复度 = int(required_rep)
 
@@ -36,13 +40,30 @@ def 来点名人名言():
     return xx
 
 def 另起一段():
-    xx = ". "
+    xx = "|。"
     xx += "\r\n"
-    xx += "    "
+    xx += "    |"
     return xx
 
 if __name__ == "__main__":
     xx = input("请输入文章主题:")
+    
+    # Users can use modified version, or the original
+    modified = input('Are you willing to generate full shit instead of bull shit? (Y)es/(N)o. No by default ').lower()
+    if modified == 'y' or modified == 'yes':
+        modified = input('How much shit would you want to eat (3 by default)? [0-9] ')
+        if modified.isdigit():
+            modified = int(modified)
+            if modified > 9:
+                modified = 9
+            elif modified < 0:
+                modified = 0
+        else:
+            modified = 3
+        modified += 1
+    else:
+        modified = 0
+
     for x in xx:
         tmp = str()
         while ( len(tmp) < 6000 ) :
@@ -53,5 +74,39 @@ if __name__ == "__main__":
                 tmp += 来点名人名言()
             else:
                 tmp += next(下一句废话)
-        tmp = tmp.replace("x",xx)
-        print(tmp)
+        tmp = tmp.split('|')
+        # Keep the original
+
+        shit = ''
+        if modified:
+            junk_length = len(tmp)
+            # Ignore punctuations and uncovered words
+            ignore_list = [0] * junk_length
+            # Count
+            shit_count = 0
+            for current_shit in range(len(tmp)):
+                if tmp[current_shit] in punctuations or tmp[current_shit] not in synonyms_keys:
+                    ignore_list[current_shit] = 1
+                else:
+                    shit_count += 1
+
+            full_shit_length = round(shit_count * modified / 10)
+            # Pollute ideal number of shit
+            for count_shit in range(full_shit_length):
+                while True:     # Trail exit when pollute well
+                    pollute_pos = random.randint(0, junk_length - 1)
+                    # Check it is need to ignore
+                    if ignore_list[pollute_pos]:
+                        continue
+                    # Gather a full mark shit
+                    current_shit = tmp[pollute_pos]
+                    full_mark_pool = synonyms[current_shit]
+                    tmp[pollute_pos] = random.choice(full_mark_pool)
+                    ignore_list[pollute_pos] = 1
+                    break
+                # Loop until enough shit be full marked
+
+        for word in tmp:
+            shit += word
+        shit = shit.replace("x", xx)
+        print(shit)
